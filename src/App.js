70d4928869c5ab1abc828fc35b19628da7a3d6d1
Lili Bourgeois <lbourgeois90@gmail.com>
2019-06-09 20:25:01 -0500

    create function to get value from button on click

--- src/App.js
+++ src/App.js
@@ -3,6 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  state={
+    value_one: '',
+    value_two: '',
+    operator: '',
+    calc_total: '',
+  }
+
+  inputValue= (event) => {
+    console.log(event.target.value);
+  }
   
   render() {
     return (
@@ -13,25 +24,25 @@ class App extends Component {
           </div>
           <div className='calc-row'>
             <button value={7} onClick={this.inputValue}>7</button>
-            <button>8</button>
-            <button>9</button>
+            <button value={8} onClick={this.inputValue}>8</button>
+            <button value={9} onClick={this.inputValue}>9</button>
             <button>/</button> 
           </div>
           <div className='calc-row'>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
+            <button value={4} onClick={this.inputValue}>4</button>
+            <button value={5} onClick={this.inputValue}>5</button>
+            <button value={6} onClick={this.inputValue}>6</button>
             <button>*</button> 
           </div>
           <div className='calc-row'>
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
+            <button value={1} onClick={this.inputValue}>1</button>
+            <button value={2} onClick={this.inputValue}>2</button>
+            <button value={3} onClick={this.inputValue}>3</button>
             <button>+</button> 
           </div>
           <div className='calc-row'>
             <button>.</button>
-            <button>0</button>
+            <button value={0} onClick={this.inputValue}>0</button>
             <button>=</button>
             <button>-</button> 
           </div>
