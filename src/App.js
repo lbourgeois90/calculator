faf40156507dfae80e353cd64fa39ea5f38366f7
Lili Bourgeois <lbourgeois90@gmail.com>
2019-06-09 20:39:20 -0500

    create operatorValue function logic to set state for operator based on operator button clicked

--- src/App.js
+++ src/App.js
@@ -25,6 +25,12 @@ class App extends Component {
     }
   }
 
+  operatorValue = (event) =>{
+    this.setState({
+      operator: event.target.value,
+    })
+  }
+
   clearValue = (event) => {
     this.setState({
       value_one: '',
@@ -33,38 +39,46 @@ class App extends Component {
       calc_total: '',
     })
   }
+
+  calculate = (event) => {
+    if(this.state.operator === '+'){
+      this.setState({
+        calc_total: (Number(this.state.value_one) + Number(this.state.value_two))
+      })
+    }
+  }
   
   render() {
-    console.log('value one is:', this.state.value_one, 'value two is:', this.state.value_two)
+    console.log('value one is:', this.state.value_one, 'value two is:', this.state.value_two, 'total is:', this.state.calc_total)
     return (
       <div className="App">
         <div className="calculator-wrapper">
         <div className='calc-row'>
-            <input></input>
+            <div>{this.state.value_one} {this.state.operator} {this.state.value_two}</div>
           </div>
           <div className='calc-row'>
             <button value={7} onClick={this.inputValue}>7</button>
             <button value={8} onClick={this.inputValue}>8</button>
             <button value={9} onClick={this.inputValue}>9</button>
-            <button>/</button> 
+            <button value={'/'} onClick={this.operatorValue}>/</button> 
           </div>
           <div className='calc-row'>
             <button value={4} onClick={this.inputValue}>4</button>
             <button value={5} onClick={this.inputValue}>5</button>
             <button value={6} onClick={this.inputValue}>6</button>
-            <button>*</button> 
+            <button value={'*'} onClick={this.operatorValue}>*</button> 
           </div>
           <div className='calc-row'>
             <button value={1} onClick={this.inputValue}>1</button>
             <button value={2} onClick={this.inputValue}>2</button>
             <button value={3} onClick={this.inputValue}>3</button>
-            <button>+</button> 
+            <button value={'+'} onClick={this.operatorValue}>+</button> 
           </div>
           <div className='calc-row'>
             <button>.</button>
             <button value={0} onClick={this.inputValue}>0</button>
-            <button>=</button>
-            <button>-</button> 
+            <button onClick={this.calculate}>=</button>
+            <button value={'-'} onClick={this.operatorValue}>-</button> 
           </div>
           <div className='calc-row'>
             <button onClick={this.clearValue}>Clear</button>
