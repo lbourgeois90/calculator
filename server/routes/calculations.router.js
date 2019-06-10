6a3a9ce549b8c711fdb1e4612ddf5227b75d0970
Lili Bourgeois <lbourgeois90@gmail.com>
2019-06-09 21:27:46 -0500

    add sql text to get

--- server/routes/calculations.router.js
+++ server/routes/calculations.router.js
@@ -5,7 +5,7 @@ const pool = require('../modules/pool');
 
 router.get('/', (req, res) => {
     console.log('in SERVER GET');
-    pool.query()
+    pool.query(`SELECT * FROM "calculator" ORDER BY "id" DESC LIMIT 10 `)
     .then((result) => {
         res.send(result.rows);
     })
@@ -19,7 +19,7 @@ router.post('/', (req, res) => {
     console.log('in SERVER POST');
     console.log('Client Calculations', req.body);
     const sqlText= `INSERT INTO "calculator" ("calc_total", "value_one", "value_two", "operator") VALUES ($1, $2, $3, $4)`;
-    pool.query( sqlText, [req.body.calc_total, req.body.value_one, req.body.value_two, req.body.operator]);
+    pool.query( sqlText, [req.body.calc_total, req.body.value_one, req.body.value_two, req.body.operator])
     .then((result) => {
         res.sendStatus(201)
     })
